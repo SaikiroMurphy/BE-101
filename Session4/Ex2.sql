d40e42d9c5364f70e7d6f10e11a813c7d2efddb8
SaikiroMurphy <ducanhn2810@gmail.com>
2025-12-03 17:57:34 +0700

    Thêm Ex2 Session 4

---
+++ Session4/Ex2.sql
@@ -0,0 +1,60 @@
+-- Tạo cơ sở dữ liệu và bảng sản phẩm, sau đó chèn một số dữ liệu mẫu vào bảng.
+CREATE DATABASE product_db;
+
+CREATE TABLE IF NOT EXISTS products (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(50),
+      category VARCHAR(50),
+      price DECIMAL(10, 2),
+      stock INT
+);
+
+INSERT INTO products (name, category, price, stock)
+VALUES ('Laptop Dell', 'Electronics', 1500.00, 5),
+      ('Chuột Logitech', 'Electronics', 25.50, 50),
+      ('Bàn phím Razer', 'Electronics', 120.00, 20),
+      ('Tủ lạnh LG', 'Home Appliances', 800.00, 3),
+      ('Máy giặt Samsung', 'Home Appliances', 600.00, 2);    
+  
+-- Thêm sản phẩm mới: 'Điều hòa Panasonic', category 'Home Appliances', giá 400.00, stock 10
+INSERT INTO products (name, category, price, stock)
+VALUES (
+            'Điều hòa Panasonic',
+            'Home Appliances',
+            400.00,
+            10
+      );
+
+-- Cập nhật stock của 'Laptop Dell' thành 7
+UPDATE products
+SET stock = 7
+WHERE name = 'Laptop Dell';
+
+-- Xóa các sản phẩm có stock bằng 0 (nếu có)
+DELETE FROM products
+WHERE stock = 0;
+
+-- Liệt kê tất cả sản phẩm theo giá tăng dần
+SELECT *
+FROM products
+ORDER BY price ASC;
+
+-- Liệt kê danh mục duy nhất của các sản phẩm (DISTINCT)
+SELECT DISTINCT category
+FROM products;
+
+-- Liệt kê sản phẩm có giá từ 100 đến 1000
+SELECT *
+FROM products
+WHERE price BETWEEN 100 AND 1000;
+
+-- Liệt kê các sản phẩm có tên chứa từ 'LG' hoặc 'Samsung' (sử dụng ILIKE)
+SELECT *
+FROM products
+WHERE name ILIKE '%LG%' OR name ILIKE '%Samsung%';
+
+-- Hiển thị 2 sản phẩm đầu tiên theo giá giảm dần
+SELECT *
+FROM products
+ORDER BY price DESC
+LIMIT 2;
